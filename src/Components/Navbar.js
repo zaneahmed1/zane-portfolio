7f45fa3078da8367357d9ddafd817285f065e41e
Zane <zane@Zanes-Air.attlocal.net>
2023-08-28 12:12:09 -0500

    add nav links and resume download

---
+++ src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Link, animateScroll as scroll} from 'react-scroll'
+import { saveAs } from 'file-saver';
+import Resume from '.././resume.pdf'
+
+export default function Navbar() {
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+    }
+    const downloadResume = () => {
+        saveAs(Resume);
+      };
+
+  return (
+    <div>
+        <ul>
+            <li onClick={scrollToTop}>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Skills</li>
+            <li onClick={downloadResume}>Resume</li>
+        </ul>
+    </div>
+  )
+}
