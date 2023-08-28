232fe8db989bfc3de5531ad2483ebd31a43beb2f
Zane <zane@Zanes-Air.attlocal.net>
2023-08-28 17:53:47 -0500

    add divs to home

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <div className='home-left'>
+        <div>Zane Ahmed</div>
+        <div>Software Engineer</div>
+        <div>Project Analyst</div>
+        </div>
+        <div className='home-right'>
+            <div className='circle-left'></div>
+            <div className='circle-right'></div>
+            </div>
+    </div>
+      )
+}
