df2bd0e0b3fa7a1ee757d161446f07290915b9f9
Zane <zane@Zanes-Air.attlocal.net>
2023-09-08 09:32:46 -0500

    Add all pages to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import About from './Components/About';
+import Skills from './Components/Skills';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <div className="App">
 <Navbar />
 <Home />
+<About/>
+<Skills/>
+<Projects/>
+<Contact/>
     </div>
   );
 }
