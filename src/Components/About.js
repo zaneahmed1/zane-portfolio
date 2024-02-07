bc918e0d4c468e4e7b40bb58561e891d0cf63d9d
Zane <zane@Zanes-Air.attlocal.net>
2024-02-07 11:26:09 -0600

    update about left

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import './About.css'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='about-container'>
+      <div className='about-left'>
+     
+      Hi, I'm Zane Ahmed, a web developer from Dallas. I love solving problems and making digital stuff easy to use. Let's work together to make the web more accessible.
+    </div>
+      <div className='about-right'>
+        <div className='row'>
+hello </div>  
+        <div className='row'>
+          hello </div>
+        <div className='row'></div>
+        hello </div>
+      </div>
+      
   )
 }
+//Add images to each row instead of the hello 
