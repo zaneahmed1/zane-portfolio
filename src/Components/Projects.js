624838aefb23213ef2648590483bc18f32eb64c0
Zane <zane@Zanes-Air.attlocal.net>
2024-02-07 15:03:47 -0600

    add projects section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import './Projects.css'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects-container'>
+<div className='project'>Project 1</div>
+<div className='project'>Project 2</div>
+<div className='project'>Project 3</div>
+    </div>
   )
 }
