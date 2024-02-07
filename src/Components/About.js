4e9cb1554e3eb1233bb44451a2d1fc4112739089
Zane <zane@Zanes-Air.attlocal.net>
2024-02-07 14:55:15 -0600

    update about with images and add skills

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,22 +1,38 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import "./About.css";
+import laptop from "../Components/programming.png"
+import speech from "../Components/text-to-speech.png"
 
 export default function About() {
   return (
-    <div className='about-container'>
-      <div className='about-left'>
-     
-      Hi, I'm Zane Ahmed, a web developer from Dallas. I love solving problems and making digital stuff easy to use. Let's work together to make the web more accessible.
-    </div>
-      <div className='about-right'>
-        <div className='row'>
-hello </div>  
-        <div className='row'>
-          hello </div>
-        <div className='row'></div>
-        hello </div>
+    <div className="about-container">
+      <div className="about-left">
+        <h3>
+        Hi, I'm Zane Ahmed, a full stack web developer based in Dallas. I love
+        solving problems and making digital stuff easy to use. Let's work
+        together to make the web more accessible.
+        </h3>
       </div>
-      
-  )
+      <div className="about-right">
+        <div className="row">
+          <img
+            src={laptop}
+            alt="image of laptop with code"
+          />
+        </div>
+        <div className="row">
+        <img
+            src={speech}
+            alt="image of text to speech tool"
+          />
+        </div>
+        <div className="row"></div>
+        <img
+            src={laptop}
+            alt="image of laptop with code"
+          />
+      </div>
+    </div>
+  );
 }
-//Add images to each row instead of the hello 
+//Add images to each row instead of the hello
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import "./Skills.css"
 
 export default function Skills() {
   return (
-    <div>Skills</div>
+    <div className='skills-container'>
+<h3>Skills</h3>
+<br/>
+<div>JavaScript</div>
+<div>HTML</div>
+<div>CSS</div>
+<div>ReactJS</div>
+<div>SQL</div>
+<div>Python</div>
+<div>Jupyter Notebook</div>
+<div>Jira</div>
+<div>Tableau</div>
+<div>Agile and Scrum Methodologies</div>
+<div>Git</div>
+    </div>
   )
 }
